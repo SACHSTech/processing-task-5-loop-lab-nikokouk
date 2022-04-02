cfca13ec5e45c53f4e9eeb2f2201228eb0510601
NikoKouk <78486979+nikokouk@users.noreply.github.com>
2022-04-02 01:54:22 +0000

    did it

--- Sketch.java
+++ Sketch.java
@@ -34,9 +34,12 @@ public class Sketch extends PApplet {
 */
     draw_section_outlines();
     draw_section1();
+    
     draw_section2();
+
     draw_section3();
     draw_section4();
+    
 	  
     draw_section5();
     draw_section6();
@@ -66,7 +69,7 @@ public class Sketch extends PApplet {
     rect(600, 0, 300, 300);
     rect(900, 0, 300, 300);
   }
-  
+    
   /**
    * draws the bottom left section
    */
@@ -76,8 +79,8 @@ public class Sketch extends PApplet {
 
     for(int intRow = 0; intRow < 30; intRow++){
       for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + intRow * 300 / 30;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 3 + 300 + intColumn * 300 / 30; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
@@ -85,6 +88,7 @@ public class Sketch extends PApplet {
 
       }
     }
+
   }
 
   /**
@@ -92,6 +96,28 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2(){
+    int twoX = 0;
+  int twoY = 0;
+
+  for(int twoRow = 0; twoRow < 30; twoRow++)
+  { 
+  for(int twoColumn = 0; twoColumn < 30; twoColumn++)
+  {
+  twoX = 3 + 300 + twoRow * 300 / 30;
+  twoY = 3 + 300 + twoColumn * 300 / 30;
+
+  if(twoRow % 2 == 0)
+  {
+    fill(255);
+    noStroke();
+    rect(twoX, twoY, 5, 5);
+  } else {
+    fill(0);
+    noStroke();
+    rect(twoX, twoY, 5, 5);
+    }
+    }
+  }
 
   }
 
@@ -101,12 +127,55 @@ public class Sketch extends PApplet {
    */
   public void draw_section3(){
 
+    int threeX = 0;
+  int threeY = 0;
+  for(int threeRow = 0; threeRow < 30; threeRow++)
+  { 
+  for(int threeColumn = 0; threeColumn < 30; threeColumn++)
+  {
+  threeX = 3 + 600 + threeRow * 300 / 30;
+  threeY = 3 + 300 + threeColumn * 300 / 30;
+
+  if(threeColumn % 2 == 0)
+  {
+    fill(255);
+    noStroke();
+    rect(threeX, threeY, 5, 5);
+  } else {
+    fill(0);
+    noStroke();
+    rect(threeX, threeY, 5, 5);
+    }
+    }
+    }
   }
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+
+    int fourX = 0;
+  int fourY = 0;
+  for(int fourRow = 0; fourRow < 30; fourRow++)
+  { 
+  for(int fourColumn = 0; fourColumn < 30; fourColumn++)
+  {
+  fourX = 3 + 900 + fourRow * 300 / 30;
+  fourY = 3 + 300 + fourColumn * 300 / 30;
+
+  if(fourColumn % 2 <= 0 && fourRow % 2 == 0)
+  {
+    fill(255);
+    noStroke();
+    rect(fourX, fourY, 5, 5);
+  } else {
+    fill(0);
+    noStroke();
+    rect(fourX, fourY, 5, 5);
+    }
+    }
+    }
 
   }
 
@@ -115,23 +184,94 @@ public class Sketch extends PApplet {
    */
   public void draw_section5(){
 
+    int fiveX = 0;
+  int fiveY = 0;
+  int fiveRows = 0;
+  int fiveColumns = 0;
+
+  for(int fiveColumn = 30; fiveColumn > fiveColumns; fiveColumn--)
+    {
+  for(int fiveRow = 30; fiveRow > fiveRows; fiveRow--)
+    {
+      fiveX = 3 - 10 + fiveRow * 300 / 30;
+      fiveY = 3 + fiveColumn * 300 / 30;
+
+      fill(255);
+      noStroke();
+      rect(fiveX, fiveY, 5, 5);
+    }
+    fiveRows += 1;
+    }
   }
 
   public void draw_section6(){
 
+    int sixX = 0;
+  int sixY = 0;
+  int sixRows = 30;
+  int sixColumns = 0;
+
+  for(int sixColumn = 30; sixColumn > sixColumns; sixColumn--)
+    {
+    for(int sixRow = 0; sixRow < sixRows; sixRow++)
+      {
+        sixX = 3 + 300 + sixRow * 300 / 30;
+        sixY = 3 - 10 + sixColumn * 300 / 30;
+        fill(255);
+        noStroke();
+        rect(sixX, sixY, 5, 5);
+      }
+      sixRows -= 1;
+    }
   }
 
   public void draw_section7(){
 
+    int sevenX = 0;
+    int sevenY = 0;
+    int sevenRows = 30;
+    int sevenColumns = 30;
+
+    for(int sevenColumn = 0; sevenColumn < sevenColumns; sevenColumn++){
+      for(int sevenRow = 0; sevenRow < sevenRows; sevenRow++)
+        {
+          sevenX = 3 + 600 + sevenRow * 300 / 30;
+          sevenY = 3 + sevenColumn * 300 / 30;
+          fill(255);
+          noStroke();
+          rect(sevenX, sevenY, 5, 5);
+
+        }
+        sevenRows -= 1;
+
+      
+    }
   }
   
   public void draw_section8(){
 
+  int eightX = 0;
+  int eightY = 0;
+  int eightRows = 0;
+  int eightColumns = 30;
+
+  for(int eightColumn = 0; eightColumn < eightColumns; eightColumn++)
+    {
+    for(int eightRow = 30; eightRow > eightRows; eightRow--)
+      {
+        eightX = 3 - 10 + 900 + eightRow * 300 / 30;
+        eightY = 3 + eightColumn * 300 / 30;
+        fill(255);
+        noStroke();
+        rect(eightX, eightY, 5, 5);
+      }
+      eightRows += 1;
+    }
+
+
+
+
+    
   }
-
-
-
-
-
 
 }
